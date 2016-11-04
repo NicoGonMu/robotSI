c5e42c3ade13b5b7b04040e7ab2be01d594649c6
oscaralonsob <oscaralonsob@gmail.com>
2016-11-04 20:52:35 +0100

    Tablero y poner muros

---
+++ Robot/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Robot {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        Tablero tablero;
+
+        public MainWindow() {
+            InitializeComponent();
+            Show();
+
+            tablero = new Tablero(18);
+            paint();
+
+
+        }
+
+        void paint()
+        {
+            TableroUI.Children.Clear();
+            for (int i = 0; i < tablero.Length; i++)
+            {
+                for (int j = 0; j < tablero.Length; j++) {
+                    Image image = new Image();
+                    int cell = tablero.getCell(i, j);
+                    if (cell == 0){
+                        var uri = new Uri("pack://application:,,,/Textures/floor.png");
+                        image.Source = new BitmapImage(uri);
+                    }else if (cell == 1){
+                        var uri = new Uri("pack://application:,,,/Textures/wall.png");
+                        image.Source = new BitmapImage(uri);
+                    }
+                    image.Width = (TableroUI.ActualHeight < TableroUI.ActualWidth - 100) ? TableroUI.ActualHeight / tablero.Length : (TableroUI.ActualWidth - 100 )/ tablero.Length;
+                    image.Height = image.Width;
+                    image.Margin = new Thickness(image.Width * i, image.Height * j, image.Width * (i + 1), image.Height * (j + 1));
+                    TableroUI.Children.Add(image);
+                }
+            }
+            
+        }
+
+        void Click(object sender, MouseEventArgs e)
+        {
+            Point point = e.GetPosition(this);
+            double size = ((TableroUI.ActualHeight < TableroUI.ActualWidth - 100) ? TableroUI.ActualHeight / tablero.Length : (TableroUI.ActualWidth - 100) / tablero.Length);
+            tablero.clickInTablero((int) (point.X / size), (int)(point.Y / size), false);
+            paint();
+        }
+    }
+}
---
+++ Robot/Tablero.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    class Tablero
+    {
+        private int length;
+        private int[,] tablero;
+
+        public int Length {
+            get {
+                return length;
+            }
+
+            set {
+                length = value;
+            }
+        }
+
+        public int getCell(int x, int y){
+            return tablero[x, y];
+        }
+
+        public Tablero(int max = 18)
+        {
+            Length = max;
+            tablero = new int[max, max];
+            for (int i = 0; i < Length; i ++){
+                for (int j = 0; j < Length; j++){
+                    tablero[i, j] = 0;
+                }
+            }
+        }
+
+        public void clickInTablero(int x, int y, bool robot){
+            if (tablero[x, y] == 0 & !robot){
+                addWall(x, y);
+            }else if (tablero[x, y] == 0){
+                addRobot(x, y);
+            }else{
+                removeCell(x, y);
+            }
+        }
+
+        private void addWall(int x, int y){
+            tablero[x, y] = 1;
+        }
+
+        private void addRobot(int x, int y){
+            tablero[x, y] = 2;
+        }
+
+        private void removeCell(int x, int y){
+            tablero[x, y] = 0;
+        }
+
+    }
+}
